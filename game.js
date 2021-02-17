2ce21a2a12f6ab5fb7ce17270cf1d83bd51b9781
2q45 <71389851+2q45@users.noreply.github.com>
2021-02-17 20:57:53 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,33 @@
+class game{
+constructor(){
+    player1 = createSprite(20,20);
+    //  player1.addImage();
+  
+      player2 = createSprite(20,20);
+     // player2.addImage();
+  
+      player3 = createSprite(20,20);
+   //   player.addImage();
+  
+      player4 = createSprite(20,20);
+       //  player1.addImage();
+  
+      player5 = createSprite(20,20);
+      //  player1.addImage();
+  
+      player6 = createSprite(20,20);
+      //  player1.addImage();
+   
+      player7 = createSprite(20,20);
+   //  player1.addImage();
+   players= [player1,player2,player3,player4,player5,player6,player7]
+  
+
+
+   play(){
+   
+
+}
+
+play(){
+}
---
+++ sketch.js
@@ -0,0 +1,13 @@
+var f;
+var player1,player2,player3,player4,player5,player6,player7,players;
+function setup() {
+  createCanvas(900,900);
+  f = new game();
+
+}
+
+function draw() {
+  background(255,255,255);  
+ //f.display();
+ drawSprites();
+}
